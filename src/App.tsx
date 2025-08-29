dcd5e35023adcca2eacad20b9f59895294bb2861
unknown <kamayabreu@gmail.com>
2025-08-28 21:14:28 -0300

    Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import OceanMenu from "@/pages/OceanMenu";
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
     <div className="w-full h-full">
       <OceanMenu />
+      <Analytics />
     </div>
   );
 }
